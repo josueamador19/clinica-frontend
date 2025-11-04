822993b56b15ab76d5abe3dc7a51f949464cd729
josueamador19 <amadoroscar789@gmail.com>
2025-11-03 18:27:44 -0600

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
+// src/services/api.js
 const isProduction = import.meta.env.MODE === "production";
 
-export const backendUrl = isProduction
-  ? import.meta.env.VITE_API_URL  
-  : "http://localhost:8000";      
+export const backendUrl =
+  isProduction
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:8000";
+console.log("API URL usada:", backendUrl);
